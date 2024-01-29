796e095d6330fa5bc6cd4c0aca3e73e8b12b7420
tzlhh <tzl5hh@qq.com>
2024-01-29 19:31:58 +0800

    123

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use http\Env\Request;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
@@ -11,4 +11,107 @@ class Company extends Model
     public $timestamps = true;
     protected $primaryKey = "id";
     protected $guarded = [];
+
+    public static function fieldcompanysearch($field)
+    {
+        try {
+            if (empty($field)) {
+                return [
+                    'status' => 'fail',
+                    'message' => '请输入查询内容',
+                    'code' => 100
+                ];
+            }
+
+            // 进行模糊查询 年级、专业、姓名、公司名称、实体虚体、申请人排名、公司规模、状态 字段
+            $result = self::where('major', 'like', "%$field%")
+                ->orWhere('stuname', 'like', "%$field%")
+                ->orWhere('grade', 'like', "%$field%")
+                ->orWhere('companyname', 'like', "%$field%")
+                ->orWhere('vp', 'like', "%$field%")
+                ->orWhere('ranking', 'like', "%$field%")
+                ->orWhere('scale', 'like', "%$field%")
+                ->orWhere('state', 'like', "%$field%")
+
+                //获取字段 年级、专业、班级、姓名、注册公司名称、实体虚体、申报人排名、注册时间、公司规模、佐证材料、状态
+                ->get(['id', 'grade', 'major', 'class', 'stuname', 'companyname', 'vp', 'ranking', 'signuptime','scale','url','state']);
+
+            if ($result->isEmpty()) {
+                return [
+                    'status' => 'fail',
+                    'message' => '未找到匹配的记录',
+                    'code' => 100
+                ];
+            }
+
+            return [
+                'status' => 'success',
+                'message' => '查询成功',
+                'data' => $result,
+                'code' => 200
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => 'fail',
+                'message' => '查询失败，发生异常: ' . $e->getMessage(),
+                'code' => 100
+            ];
+        }
+    }
+    public static function Tzlcompany($grade, $major, $class, $stuname, $stuid, $companyname, $vp, $ranking, $signuptime, $scale, $url)
+    {
+        try {
+            $res = Company::create([
+                'grade' => $grade,
+                'major' => $major,
+                'class' => $class,
+                'stuname' => $stuname,
+                'stuid' => $stuid,
+                'companyname' => $companyname,
+                'vp' => $vp,
+                'ranking' => $ranking,
+                'signuptime' => $signuptime,
+                'scale' => $scale,
+                'url' => $url,
+            ]);
+            return $res;
+        } catch (\Exception $e) {
+            return 'error ' . $e->getMessage();
+        }
+    }
+
+    public static function Tzlgetcompany($stuid){
+        try {
+            $res = Company::where('stuid',$stuid)->get();
+            return $res;
+        }catch (\Exception $e){
+            return 'error ' . $e->getMessage();
+        }
+    }
+
+    public static function Tzleditcompany($id,$companyname,$vp,$ranking,$signuptime,$scale,$url){
+        try {
+            $res = Company::where('id',$id)->update([
+                'companyname' => $companyname,
+                'vp' => $vp,
+                'ranking' => $ranking,
+                'signuptime' => $signuptime,
+                'scale' => $scale,
+                'url' => $url,
+            ]);
+            return $res;
+        }catch (\Exception $e){
+            return 'error ' . $e->getMessage();
+        }
+    }
+
+    public static function Findcompany($id)
+    {
+        try {
+            $state = Company::where('id', $id)->value('state');
+            return $state;
+        } catch (\Exception $e) {
+            return 'error ' . $e->getMessage();
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\TzlController;
+use App\Http\Controllers\ZlController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +23,21 @@ Route::middleware(['jwt.role:users','cors'])->prefix('stu')->group(function () {
 
 });
 Route::middleware(['jwt.role:admins','cors'])->prefix('admin')->group(function () {
-
 });
 
+Route::middleware(['cors'])->prefix('admin')->group(function () {
+    Route::get('getlearning',[TzlController::class,'Tzlgetlearning']); //获取进步之星或学习之星
+    Route::get('getcompetition',[TzlController::class,'Tzlgetcompetition']); //获取竞赛之星数据
+    Route::post('competitionsearch',[TzlController::class,'Tzlcompetitionsearch']); //竞赛之星搜索
+    Route::get('getsci',[TzlController::class,'Tzlgetsci']); //获取科研之星数据
+    Route::post('scisearch',[TzlController::class,'Tzlscisearch']); //科研之星搜索
+    Route::get('getcompany',[TzlController::class,'Tzlgetcompany']); //双创之星数据
+    Route::post('companysearch',[TzlController::class,'Tzlcompanysearch']); //双创之星搜索
+});
+Route::middleware(['cors'])->prefix('stu')->group(function () {
+    Route::post('commitcompany',[ZlController::class,'Tzlcommitcompany']); //提交双创之星表单
+    Route::get('getcompany',[ZlController::class,'Tzlgetcompany']); //获取个人双创之星表单
+    Route::post('editcompany',[ZlController::class,'Tzleditcompany']); //修改双创之星
+    Route::get('getreasonShuangc',[ZlController::class,'TzlgetreasonShuangc']); //查询驳回理由
+    Route::post('delete',[ZlController::class,'Tzldelete']); //删除某项表单双创
+});
